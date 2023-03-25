98d85a86c5e378e2784b111f186c1a47d5e73757
NastasiaMazur <nvmazour@mail.ru>
2023-03-26 00:25:36 +0100

    initial commit

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def dict_func(my_dict):
 
 my_dict = {"Type": "laptop", "Brand": "Huawei", "Price": 700}
 #my_dict = {"Type": "laptop", "Price": 700}
-new_dict = dict_func(my_dict)
+dict_func(my_dict)
